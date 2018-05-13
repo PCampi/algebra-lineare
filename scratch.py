9432158d158dc51519c6c334df026c7fe84ca844
Pietro Mattia Campi <pietro.campi.dev@gmail.com>
2018-05-13 10:39:08 +0200

    Aggiunto memory profiling.
    TODO: solo logging su file csv

--- scratch.py
+++ scratch.py
@@ -1,11 +1,20 @@
 """Scratch file."""
 
 import datetime
+import os
 
 import numpy as np
+import psutil
 import scipy as sp
 import scipy.io as sio
 import scipy.sparse.linalg as slinalg
+
+import tqdm
+
+
+class InvalidMatrixFormat(Exception):
+    """Exception raised if a matrix is in an invalid format."""
+    pass
 
 
 def load_mm_matrix(name):
@@ -47,7 +56,8 @@ def solve_system(A, b, umfpack=True):
     Parameters
     ----------
     A: scipy.sparse matrix
-        coefficients matrix
+        coefficients matrix, MUST be in csr or csc format, otherwise
+        will throw an exception
     
     b: numpy.array of shape(A.shape[1], 1)
         right hand side of the system that should cause the exact solution
@@ -62,34 +72,37 @@ def solve_system(A, b, umfpack=True):
             'memory': memory used during computation
             'solver_library': 'umfpack' or 'superlu'
     """
-    # 1. convert matrix to csr format before solving
+    # 1. check if matrix is in right format, else raise exception
     a_format = A.getformat()
     if a_format not in {'csr', 'csc'}:
-        A = A.tocsr()
-        print("Matrix converted from '{}' format to 'csr' format.".format(
-            a_format))
+        raise InvalidMatrixFormat(
+            "Matrix is in format '{}', but only 'csr' and 'csc' are supported".
+            format(a_format))
 
-    # TODO: insert memory profiling here and after solving the system
+    current_process = psutil.Process(os.getpid())
+
     start_time = datetime.datetime.now()
+    start_memory = current_process.memory_info()
+
     x = slinalg.spsolve(A, b, use_umfpack=umfpack)
+
+    end_memory = current_process.memory_info()
     end_time = datetime.datetime.now()
 
-    if umfpack:
-        solver_library = 'umfpack'
-    else:
-        solver_library = 'superlu'
+    solver_library = 'umfpack' if umfpack else 'superlu'
 
     result = {
         'x': x,
         'elapsed_time': end_time - start_time,
-        'memory': 1,
+        'memory_physical': end_memory.rss - start_memory.rss,
+        'memory_virtual': end_memory.vms - start_memory.vms,
         'solver_library': solver_library,
     }
 
     return result
 
 
-def solve_with_profiling(A, b, n_times=1, umfpack=True):
+def solve_with_profiling(A, b, n_times=1, umfpack=True, progress=True):
     """Perform a benchmark on the given matrix-rhs for solving A*xe = b,
     where xe is assumed to be a vector of ones [1, 1,..., 1].T
 
@@ -108,19 +121,43 @@ def solve_with_profiling(A, b, n_times=1, umfpack=True):
     umfpack: bool
         wether to use umfpack as a solver, default True (it's way faster)
     
+    progress: bool
+        if True, show iterations with a progress bar, else only print
+    
     Returns
     -------
-
+    result: Dict
+        dictionary with these key-value pairs:
+            'elapsed_time': List[int], list of elapsed times for each iteration
+            'memory_physical': List[int], list of physical memory used per iteration (bytes)
+            'memory_virtual': List[int], list of virtual memory used per iteration (bytes)
+            'relative_error': List[float], list of relative error computed as norm2(xe - x)/norm2(xe)
     """
-    results = [solve_system(A, b, umfpack=umfpack) for _ in range(n_times)]
+    a_format = A.getformat()
+    if a_format not in {'csr', 'csc'}:
+        print("Matrix is in '{}' format, converting to 'csr'".format(a_format))
+        A = A.tocsr()
+        print("Matrix format converted")
 
-    xe = np.ones((A.shape[1], 1))
+    if progress:
+        results = [solve_system(A, b) for i in tqdm.tqdm(range(n_times))]
+    else:
+        # define a custom iteration function
+        def iter_fun(it, total):
+            """Iteration function which prints some useful info."""
+            print("iteration {}/{}".format(it, total))
+            return solve_system(A, b)
+
+        results = [iter_fun(it + 1, n_times) for it in range(n_times)]
+
+    xe = np.ones((A.shape[1], ))
 
     elapsed_time = [
         result['elapsed_time'].total_seconds() for result in results
     ]
 
-    memory_used = [result['memory'] for result in results]
+    physical_memory = [result['memory_physical'] for result in results]
+    virtual_memory = [result['memory_virtual'] for result in results]
 
     relative_error = [
         get_relative_error(xe, result['x']) for result in results
@@ -128,6 +165,16 @@ def solve_with_profiling(A, b, n_times=1, umfpack=True):
 
     return {
         'elapsed_time': elapsed_time,
-        'memory': memory_used,
+        'memory_physical': physical_memory,
+        'memory_virtual': virtual_memory,
         'relative_error': relative_error,
     }
+
+
+if __name__ == '__main__':
+    A = load_mm_matrix('cfd1.mtx')
+    b = create_b(A)
+
+    print("Solving...")
+    result = solve_with_profiling(A, b)
+    print("Done!")
