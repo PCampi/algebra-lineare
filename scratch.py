b547d212306f652afb6535ad1809ea94d0868487
Pietro Mattia Campi <pietro.campi.dev@gmail.com>
2018-05-15 19:47:59 +0200

    Fatta la parte per simmetriche definite positive.
    Usata Intel MKL con Anaconda.
    
    TODO: risultati su csv e finito.

--- scratch.py
+++ scratch.py
@@ -7,6 +7,7 @@ import gc
 
 import numpy as np
 import psutil
+from pypardiso import spsolve as mkl_spsolve
 import scipy as sp
 import scipy.io as sio
 import scipy.sparse.linalg as slinalg
@@ -30,9 +31,9 @@ def load_matrix(path):
     Returns
     -------
     m: scipy.sparse matrix
-        the loaded matrix
+        the loaded matrix in 'csr' format
     """
-    return sio.mmread(path)
+    return sio.mmread(path).tocsr()
 
 
 def create_b(matrix):
@@ -51,19 +52,22 @@ def get_relative_error(xe, x):
     return relative_error
 
 
-def solve_system(A, b, umfpack=True):
+def solve_system(A, b, mkl=True):
     """Solve a sparse linear system with coefficients matrix A and
     rhs b.
 
     Parameters
     ----------
     A: scipy.sparse matrix
-        coefficients matrix, MUST be in csr or csc format, otherwise
-        will throw an exception
+        coefficients matrix, for best performance must be in csr format
     
     b: numpy.array of shape(A.shape[1], 1)
         right hand side of the system that should cause the exact solution
         to be all ones
+    
+    mkl: boolean
+        if True, use the Pardiso solver from the Intel MKL library to speedup
+        computation, otherwise try with umfpack/superLU
     
     Returns
     -------
@@ -72,19 +76,24 @@ def solve_system(A, b, umfpack=True):
             'x': result of the computation
             'elapsed_time': time spent during computation
             'memory': memory used during computation
-            'solver_library': 'umfpack' or 'superlu'
+            'solver_library': 'mkl' or 'superlu'
     """
     current_process = psutil.Process(os.getpid())
+
+    if mkl:
+        solve_function = mkl_spsolve
+    else:
+        solve_function = slinalg.spsolve
 
     start_time = datetime.datetime.now()
     start_memory = current_process.memory_info()
 
-    x = slinalg.spsolve(A, b, use_umfpack=umfpack)
+    x = solve_function(A, b)
 
     end_time = datetime.datetime.now()
     end_memory = current_process.memory_info()
 
-    solver_library = 'umfpack' if umfpack else 'superlu'
+    solver_library = 'mkl' if mkl else 'superlu'
 
     result = {
         'x': x,
@@ -97,7 +106,7 @@ def solve_system(A, b, umfpack=True):
     return result
 
 
-def solve_with_profiling(A, b, n_times=1, umfpack=True, progress=True):
+def solve_with_profiling(A, b, n_times=1, mkl=True, progress=True):
     """Perform a benchmark on the given matrix-rhs for solving A*xe = b,
     where xe is assumed to be a vector of ones [1, 1,..., 1].T
 
@@ -113,8 +122,8 @@ def solve_with_profiling(A, b, n_times=1, umfpack=True, progress=True):
     n_times: int
         number of times that the test will be repeated, default 1
     
-    umfpack: bool
-        wether to use umfpack as a solver, default True (it's way faster)
+    mkl: bool
+        if True use Intel MKL Pardiso solver, otherwise SuperLU (waaaay slower...)
     
     progress: bool
         if True, show iterations with a progress bar, else only print
@@ -127,15 +136,10 @@ def solve_with_profiling(A, b, n_times=1, umfpack=True, progress=True):
             'memory_physical': List[int], list of physical memory used per iteration (bytes)
             'memory_virtual': List[int], list of virtual memory used per iteration (bytes)
             'relative_error': List[float], list of relative error computed as norm2(xe - x)/norm2(xe)
+            'solver_library': List[str], list of values of the solver library
     """
-    a_format = A.getformat()
-    if a_format not in {'csr', 'csc'}:
-        raise InvalidMatrixFormat(
-            "Matrix is in format '{}', but only 'csr' and 'csc' are supported".
-            format(a_format))
-
-    print("Starting benchmark with {} runs using {}".format(
-        n_times, 'umfpack' if umfpack else 'superlu'))
+    print("Starting benchmark with {} runs using {}:".format(
+        n_times, 'Intel MKL' if mkl else 'SuperLU'))
 
     if progress:
         results = [solve_system(A, b) for i in tqdm.tqdm(range(n_times))]
@@ -161,8 +165,10 @@ def solve_with_profiling(A, b, n_times=1, umfpack=True, progress=True):
         get_relative_error(xe, result['x']) for result in results
     ]
 
+    solver = [result['solver_library'] for result in results]
+
     del xe
-    print("Collecting memory...")
+    print("Collecting old xe...")
     gc.collect()
     print("GC collection finished")
 
@@ -171,6 +177,7 @@ def solve_with_profiling(A, b, n_times=1, umfpack=True, progress=True):
         'memory_physical': physical_memory,
         'memory_virtual': virtual_memory,
         'relative_error': relative_error,
+        'solver_library': solver,
     }
 
 
@@ -187,42 +194,30 @@ def main_sdf():
     for index, path in enumerate(matrices_sdf):
         matrix_name = path.split('/')[-1][:-4]
         A = load_matrix(path)
-        b = create_b(A)
+
+        if A.getformat() != 'csr':
+            raise InvalidMatrixFormat(
+                "Matrix is in format '{}', but only 'csr' is supported".format(
+                    A.getformat()))
+
         print("\n\n## ---------------------- ##")
-        print("Solving for matrix {}/{}: {}".format(index, len(matrices_sdf),
-                                                    matrix_name))
+        print("Solving for matrix {} ({}/{})".format(matrix_name, index + 1,
+                                                     len(matrices_sdf)))
         print("Matrix dimension: {}".format(A.shape))
 
-        a_format = A.getformat()
-        # convert but try to save some memory
-        if a_format not in {'csr', 'csc'}:
-            print("Matrix is in '{}' format, converting to 'csr'".format(
-                a_format))
-            Mat = A.tocsr()
-            print("Matrix format converted")
+        b = create_b(A)
+        result = solve_with_profiling(A, b)
 
-            del A
-            print("Collecting old matrix...")
-            gc.collect()
-            print("GC collection finished")
-
-            result = solve_with_profiling(Mat, b)
-
-            del b
-            print("Collecting old b...")
-            gc.collect()
-            print("GC collection finished")
-        else:
-            result = solve_with_profiling(A, b)
-            del A, b
-            print("Collecting old A and b...")
-            gc.collect()
-            print("GC collection finished")
+        del A, b
+        print("Collecting old A and b...")
+        gc.collect()
+        print("GC collection finished")
 
         results[matrix_name] = result
         print("Done with matrix {}".format(matrix_name))
 
     print("Done!")
+    return results
 
 
 if __name__ == '__main__':
