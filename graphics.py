9941e936d721a4bbf620065d8cb538db288f7c56
Pietro Mattia Campi <pietro.campi.dev@gmail.com>
2018-05-31 17:20:05 +0200

    Aggiunto notebook per grafici. WIP.

--- graphics.py
+++ graphics.py
@@ -1,6 +1,24 @@
 """Documento per grafici del progetto 1."""
 import pandas as pd
+import numpy as np
 from matplotlib import pyplot as plt
+
+MATRIX_NNZ = {
+    'graham1.mtx': 335504,
+    'raefsky3.mtx': 1488768,
+    'ex15.mtx': 98671,
+    'ex19.mtx': 259879,
+    'torso1.mtx': 8516500,
+    'water_tank.mtx': 2035281,
+    'cfd1.mtx': 1828364,
+    'PR02R.mtx': 8185136,
+    'cfd2.mtx': 3087898,
+    'torso3.mtx': 4429042,
+    'shallow_water1.mtx': 327680,
+    'parabolic_fem.mtx': 3674625,
+    'apache2.mtx': 4817870,
+    'G3_circuit.mtx': 7660826
+}
 
 
 def calculate_col_v_memory_mean(t_start, t_stop, mem_log):
@@ -44,22 +62,63 @@ def calculate_mem_maxmin(matrix_name, data) -> int:
     return used_mem
 
 
-def create_python_dataframe(log, memory_log):
-    python_dataframe = 0  #Da definire!
-    current_mem_val = 0
+def create_python_dataframe(run_log, memory_log):
+    """Create a result DataFrame for Python."""
+    columns = [
+        'matrix', 'dimensions', 'type', 'iter', 'times_mean', 'times_var',
+        'rel_error', 'system', 'memory'
+    ]
+    new_df = pd.DataFrame(columns=columns)
 
-    mem_values = []
-    for index, row in log.iterrows():
-        current_mem_val = calculate_col_p_memory_maxmin(
-            row[3], row[4], memory_log)
-        mem_values.append(current_mem_val)
-    log['memory'] = mem_values
+    matrix_names = np.unique(run_log['matrix'])
+    dimensions = [
+        run_log[run_log['matrix'] == name]['dimensions'].iloc[0]
+        for name in matrix_names
+    ]
+    types = [
+        run_log[run_log['matrix'] == name]['type'].iloc[0]
+        for name in matrix_names
+    ]
+    rel_error = [
+        run_log[run_log['matrix'] == name]['rel_error'].iloc[0]
+        for name in matrix_names
+    ]
+    system = [
+        'Ubuntu' if run_log['system'].iloc[0] == 'ubuntu' else 'Windows'
+    ] * len(matrix_names)
+    iters = [30] * len(matrix_names)
 
-    #FUNZIONE NON COMPLETA: manca da riuscire a raggruppare i dati delle matrici, ora sparsi per iterazione, e computare:
-    # time_mean, time_var, memory, rel_error
-    #Bisogna poi creare il nuovo dataframe completo da poter poi utilizzare per la costruzione dei grafi
+    memory_mean = []
+    memory_var = []
+    times_mean = []
+    times_var = []
+    for name in matrix_names:
+        start_times = run_log[run_log['matrix'] == name]['start_time']
+        end_times = run_log[run_log['matrix'] == name]['end_time']
+        elapsed = (end_times - start_times).values
+        times_mean.append(np.mean(elapsed))
+        times_var.append(np.var(elapsed))
+        mem = []
+        for start_time, end_time in zip(start_times, end_times):
+            m = memory_log.query(
+                'timestamp >= @start_time and timestamp <= @end_time')
+            used_memory = m['memory_physical(kB)'].max()\
+                        - m['memory_physical(kB)'].min()
+            mem.append(used_memory)
+        memory_mean.append(np.mean(mem))
+        memory_var.append(np.var(mem))
 
-    return python_dataframe
+    new_df.loc[:, 'matrix'] = matrix_names
+    new_df.loc[:, 'dimensions'] = dimensions
+    new_df.loc[:, 'type'] = types
+    new_df.loc[:, 'iter'] = iters
+    new_df.loc[:, 'times_mean'] = times_mean
+    new_df.loc[:, 'times_var'] = times_var
+    new_df.loc[:, 'rel_error'] = rel_error
+    new_df.loc[:, 'system'] = system
+    new_df.loc[:, 'memory'] = memory_mean
+
+    return new_df
 
 
 def create_graphics():
@@ -77,7 +136,7 @@ def create_graphics():
         sep=',',
         encoding="utf-8-sig")
 
-    ubuntu_python_log = pd.read_csv(
+    ubuntu_python_all_runs = pd.read_csv(
         "./log_finali/ubuntu_python_result_log.csv",
         sep=',',
         encoding="utf-8-sig")
@@ -100,7 +159,7 @@ def create_graphics():
         sep=',',
         encoding="utf-8-sig")
 
-    windows_python_log = pd.read_csv(
+    windows_python_all_runs = pd.read_csv(
         "./log_finali/windows_python_result_log.csv",
         sep=',',
         encoding="utf-8-sig")
@@ -128,16 +187,22 @@ def create_graphics():
 
     windows_matlab_log['memory'] = [
         calculate_mem_maxmin(matrix_name, windows_matlab_times)
-        for matrix_name in ubuntu_matlab_log['matrix']
+        for matrix_name in windows_matlab_log['matrix']
     ]
+
+    ubuntu_matlab_log.to_csv('./dati_grafici/matlab_ubuntu.csv')
+    windows_matlab_log.to_csv('./dati_grafici/matlab_windows.csv')
 
     print("Aggiunta memoria completata")
 
     #Preparazione dati Python
-    ubuntu_python_final = create_python_dataframe(ubuntu_python_log,
+    ubuntu_python_final = create_python_dataframe(ubuntu_python_all_runs,
                                                   ubuntu_python_memory)
-    windows_python_final = create_python_dataframe(windows_python_log,
+    windows_python_final = create_python_dataframe(windows_python_all_runs,
                                                    windows_python_memory)
+
+    ubuntu_python_final.to_csv('./dati_grafici/python_ubuntu.csv')
+    windows_python_final.to_csv('./dati_grafici/python_windows.csv')
 
     #Da qui i grafici di matlab possono essere fatti solo con ubuntu_matlab_log e windows_matlab_log
     #PS C'è sempre il problema che alcune matrici sembrano aver girato utilizzando zero memoria
